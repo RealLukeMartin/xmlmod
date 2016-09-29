87f02b32d5604f5de7af6c764b0b09d959c10e5c
RealLukeMartin <lukemartinwebdev@gmail.com>
2016-09-29 13:50:15 -0500

    abstract replacement string

--- modi.php
+++ modi.php
@@ -5,12 +5,13 @@ $doc->load('testerbk.xml');
 
 $xPath = new DOMXPath($doc);
 $char = ", ";
+$rep = "</keyword><keyword>";
 
 $docElements = $xPath->query('/nodes/node/Keywords');
 
 foreach ($docElements as $docElement) {
 	$values = $docElement->nodeValue;
-	$str = str_replace($char, "</keyword><keyword>", $values);
+	$str = str_replace($char, $rep, $values);
 	print $str;
     $values = $str;
 	$docElement->nodeValue = $str;
